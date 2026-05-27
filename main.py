14f851f42e8afce355d6f28f5bf1dd55ca83f16b
Andres ramirez <157803607+andres-ramirez25@users.noreply.github.com>
2026-05-26 22:06:32 -0500

    prueba n

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ app = FastAPI(title="Dann-Alpes Reseñas API", version="1.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
